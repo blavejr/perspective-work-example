c32f9e142e88bad5de99a0ce71ab1f559d0177d1
remigius.kalimba <remigius.kalimba@flexperto.com>
2024-03-14 11:39:30 +0100

    user schema email must be unique

--- src/infrastructure/outbound/database/mongoose/user.model.ts
+++ src/infrastructure/outbound/database/mongoose/user.model.ts
@@ -1,9 +1,8 @@
 import { Schema, model, Document } from 'mongoose';
 import User from '../../../../domain/entities/user';
-import { v4 as uuidv4 } from 'uuid';
 
 const userSchema = new Schema({
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true},
     password: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
